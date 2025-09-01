a9a031f3e160ac5a12b5a369a73092cbda8123ff
N-Dave-08 <cdave.dev@gmail.com>
2025-09-02 05:08:21 +0800

    refactor: streamline API exports by consolidating admin and user service imports

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,11 +1,10 @@
 // Export the main API client
 export { default as api } from "./client";
-
+export * from "./services/admin";
 // Export all API services
 export * from "./services/auth";
 export * from "./services/posts";
-export * from "./services/user";
-export * from "./services/admin";
 export * from "./services/test-api";
+export * from "./services/user";
 // Export all types
 export * from "./types";
